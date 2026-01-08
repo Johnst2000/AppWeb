1d345e6868f1624340e88f10ddd47fa583e280cc
aldair xd <herreraaldair111@gmail.com>
2026-01-07 23:17:19 -0500

    Frontend

---
+++ backend/AppWeb-back/src/test/java/org/uteq/appwebback/AppWebBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uteq.appwebback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
